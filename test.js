18f2397cb18976e9fac5b1e4b3d85cd980cd4dea
David Trejo <david.daniel.trejo@gmail.com>
2010-11-30 03:50:13 -0500

    better test, improved readme

--- test.js
+++ test.js
@@ -2,8 +2,10 @@ var favicon = require('./favicon');
 
 // testing
 var domains = ['google.com', 'amazon.com', 'getzazu.com', 'brown.edu'];
+console.log('testing on');
+console.log(domains);
 for (var i in domains){
   favicon.get(domains[i], function(err, furl) {
-    console.log('> ', furl);
+    console.log('>', furl);
   }); 
 }
